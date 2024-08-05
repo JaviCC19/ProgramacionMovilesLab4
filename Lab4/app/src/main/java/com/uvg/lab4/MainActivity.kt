60653fab4f09b4715d0f1a26a068b9f330b1511b
JaviCC19 <133834053+JaviCC19@users.noreply.github.com>
2024-08-04 19:23:50 -0600

    Update

--- Lab4/app/src/main/java/com/uvg/lab4/MainActivity.kt
+++ Lab4/app/src/main/java/com/uvg/lab4/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.uvg.lab4.layouts.Front
 import com.uvg.lab4.ui.theme.Lab4Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Front(modifier = Modifier.padding( innerPadding))
+                    
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab4Theme {
-        Greeting("Android")
     }
 }
